428e90cae061101c9f2c8c12f8a33e7dc07e5d63
jibundeyare <contact@jibundeyare.com>
2022-01-06 10:53:54 +0100

    Amélioration du code
    - ajout de code de conditions

--- conditions.php
+++ conditions.php
@@ -24,3 +24,70 @@ if ($isWaterOff && $isGasOff) {
         echo 'Il faut couper le gaz<br>';
     }
 }
+
+$hasPaymentCard = (bool) random_int(0, 1);
+$hasCash = (bool) random_int(0, 1);
+
+dump($hasPaymentCard); // false
+dump($hasCash); // false
+
+if ($hasPaymentCard || $hasCash) {
+    echo "Vous pouvez partir faire les courses<br>\n";
+
+    if (!$hasPaymentCard) {
+        echo "Il vous manque la carte de paiement<br>\n";
+    }
+
+    if (!$hasCash) {
+        echo "Il vous manque du liquide<br>\n";
+    }
+} else {
+    echo "Vous ne pouvez pas partir faire les courses<br>\n";
+    echo "Vous n'avez aucun moyen de paiement<br>\n";
+}
+
+$stock = random_int(0, 10);
+
+dump($stock);
+
+if ($stock == 0) {
+    echo "Rupture de stock<br>\n";
+} elseif ($stock == 1) {
+    echo "Il reste {$stock} pièce<br>\n";
+} elseif ($stock <= 3) {
+    echo "Il reste {$stock} pièces<br>\n";
+} else {
+    echo "Stock disponible<br>\n";
+}
+
+$group = random_int(0, 3);
+
+dump($group);
+
+switch ($group) {
+    case 1:
+        echo "Vous êtes dans le premier groupe<br>\n";
+        break;
+    case 2:
+        echo "Vous êtes dans le deuxième groupe<br>\n";
+        break;
+    case 3:
+        echo "Vous êtes dans le troisième groupe<br>\n";
+        break;
+    default:
+        echo "Vous n'êtes dans aucun groupe<br>\n";
+}
+
+switch (true) {
+    case $group == 1:
+        echo "Vous êtes dans le premier groupe<br>\n";
+        break;
+    case $group == 2:
+        echo "Vous êtes dans le deuxième groupe<br>\n";
+        break;
+    case $group == 3:
+        echo "Vous êtes dans le troisième groupe<br>\n";
+        break;
+    default:
+        echo "Vous n'êtes dans aucun groupe<br>\n";
+}
