1b2fd5160b263586f413ad97d9389871db49410b
jibundeyare <contact@jibundeyare.com>
2022-01-07 12:31:22 +0100

    Amélioration du code

--- variables.php
+++ variables.php
@@ -48,3 +48,29 @@ unset($referundum);
 // car la variable a été déréférencée
 // echo $referundum;
 // echo '<br>';
+
+// transtypage de données d'une chaîne de caractères en nombre entier
+// le type de données de destination doit être mis entre parenthèses
+$numericValue = (int) "123 foo bar baz";
+// même action mais avec une fonction
+$numericValue = intval("123 foo bar baz");
+
+// transtypage de données d'un nombre entier en nombre à virgule flottante
+$numericValue = (float) 123;
+// même action mais avec une fonction
+$numericValue = floatval(123);
+
+// transtypage de données d'un nombre à virgule flottante en nombre entier
+// ATTENTION : la conversion va supprimer les chiffres après la virgule donc cette opération fait perdre une partie de l'information
+$numericValue = (int) 3.14;
+// même action mais avec une fonction
+$numericValue = intval(3.14);
+
+// transtypage de données d'un nombre entier en booléen
+$booleanValue = (bool) 123;
+// même action mais avec une fonction
+$booleanValue = boolval(123);
+
+// voici la liste (presque complète) des types de données : bool, float, int, string, array
+
+// voici la liste complète des fonctions de transtypage : boolval(), floatval(), intval(), strval()
