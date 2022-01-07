b032371804f99ac60789364fbb2d062503c4496f
jibundeyare <contact@jibundeyare.com>
2022-01-07 12:35:07 +0100

    Amélioration d'un exercice

--- exo-02-arrays.php
+++ exo-02-arrays.php
@@ -12,16 +12,16 @@ Rappel des valeurs de certaines cartes :
 - dame : 12
 - roi : 13
 
-Stockez la taille du tableau dans une variable.
+Stockez la taille du tableau dans une variable en utilisant la fonction count().
 
-Tirez deux nombres au hasard, compris entre 1 et la taille du tableau inclus, en utilisant la fonction random_int().
-Stockez ces nombres dans un tableau.
+Tirez un nombre au hasard, compris entre 1 et la taille du tableau inclus, en utilisant la fonction random_int().
+Stockez ce nombre dans un tableau.
 
-Si le même nombre a été tiré deux fois, affichez un message qui dit que le même nombre a été tiré, puis arrêtez le programme avec la fonction exit().
+À l'aide de ce nombres, retirer une carte de la main.
 
-À l'aide de ces nombres, retirer deux cartes de la main.
+Recommencez afin de retirer une deuxième carte au hasard.
 
-Affichez les nombres tirés au hasard et le nom des cartes qui ont été retirées.
+Affichez le nombres tirés au hasard et le nom des cartes qui ont été retirées.
 
 Comparez les valeurs numériques des cartes et affichez quelle carte a une valeur supérieur ou s'il y a égalité.
 Attention, par convention la carte "as" (valeur 1) est la plus élevée.
