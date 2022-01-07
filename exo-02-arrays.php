e773abe9dc0b4f5b2460bde6289609c3aef3d103
jibundeyare <contact@jibundeyare.com>
2022-01-07 14:09:28 +0100

    Amélioration d'un exercice

--- exo-02-arrays.php
+++ exo-02-arrays.php
@@ -26,3 +26,13 @@ Affichez le nombres tirés au hasard et le nom des cartes qui ont été retirée
 Comparez les valeurs numériques des cartes et affichez quelle carte a une valeur supérieur ou s'il y a égalité.
 Attention, par convention la carte "as" (valeur 1) est la plus élevée.
 */
+
+// bonus
+// Si vous voulez afficher correctement le nom des cartes, vous pouvez utiliser la fonction preg_replace().
+// Exemple qui remplace 1 par as
+// 1 carreau sera remplacé par "as carreau"
+// mais 11 carreau ne sera pas remplacé
+// et 9 treffle ne sera pas remplacé non plus
+$nomCarte = preg_replace('/^1 /', 'as', '1 carreau');
+$nomCarte = preg_replace('/^1 /', 'as', '11 carreau');
+$nomCarte = preg_replace('/^1 /', 'as', '9 treffle');
