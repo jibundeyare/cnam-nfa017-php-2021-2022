e4c839040f1c69b4480db78c29e6de8ecb9c1f17
jibundeyare <contact@jibundeyare.com>
2022-01-07 09:56:22 +0100

    Amélioration d'un exercice

--- exo-01-arrays.php
+++ exo-01-arrays.php
@@ -5,7 +5,7 @@ Creéz un tableau contenant 5 cartes d'un jeu de 32 cartes à jouer.
 
 Exemple de nom de cartes : 'As pique', '3 carreau', 'Valet treffle', etc.
 
-Tirez un nombre "n" au hasard, compris entre 1 et 5 inclus.
+Tirez un nombre "n" au hasard, compris entre 1 et 5 inclus en utilisant la fonction random_int().
 
 À l'aide de ce nombre, retirer la "n-ième" carte de la main.
 
