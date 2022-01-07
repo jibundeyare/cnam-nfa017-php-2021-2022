b236e8e7ec08c1a722553c61567747df21462f6c
jibundeyare <contact@jibundeyare.com>
2022-01-07 11:47:59 +0100

    Ajout d'un exercice

---
+++ exo-02-arrays.php
@@ -0,0 +1,26 @@
+<?php
+
+/*
+Creéz un tableau contenant 5 cartes d'un jeu de 32 cartes à jouer.
+
+Exemple de nom de cartes : '1 pique', '3 carreau', '11 treffle', etc.
+
+Rappel des valeurs de certaines cartes :
+
+- As : 1
+- Valet : 11
+- Dame : 12
+- Roi : 13
+
+Stockez la taille du tableau dans une variable.
+
+Tirez deux nombres au hasard, compris entre 1 et la taille du tableau inclus, en utilisant la fonction random_int().
+Stockez ces nombres dans un tableau.
+
+À l'aide de ces nombres, retirer deux cartes de la main.
+
+Affichez les nombre tirés au hasard et le nom des cartes qui ont été retirées.
+
+Comparez les valeurs des cartes et affichez quelle carte a une valeur supérieur.
+Par convention, la carte As (valeur 1) est la plus élevée.
+*/
