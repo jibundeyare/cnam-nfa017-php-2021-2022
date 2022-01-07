8e6aa24b146bb1d2bf3a91d483dce6f168098b1f
jibundeyare <contact@jibundeyare.com>
2022-01-07 09:39:00 +0100

    Ajout d'un exercice

---
+++ exo-01-arrays.php
@@ -0,0 +1,13 @@
+<?php
+
+/*
+Creéz un tableau contenant 5 cartes d'un jeu de 32 cartes à jouer.
+
+Exemple de nom de cartes : 'As pique', '3 carreau', 'Valet treffle', etc.
+
+Tirez un nombre "n" au hasard, compris entre 1 et 5 inclus.
+
+À l'aide de ce nombre, retirer la "n-ième" carte de la main.
+
+Affichez le nom de la carte qui a été retirée.
+*/
