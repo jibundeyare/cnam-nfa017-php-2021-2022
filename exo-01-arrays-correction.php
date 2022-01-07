c29dffdc4da37a404f223699b8e2b82ae7d8159a
jibundeyare <contact@jibundeyare.com>
2022-01-07 16:04:53 +0100

    Amélioration d'un exercice

--- exo-01-arrays-correction.php
+++ exo-01-arrays-correction.php
@@ -11,3 +11,24 @@ Tirez un nombre "n" au hasard, compris entre 1 et 5 inclus, en utilisant la fonc
 
 Affichez le nombre tiré au hasard et le nom de la carte qui a été retirée.
 */
+
+require __DIR__.'/vendor/autoload.php';
+
+$cards = [
+    'as pique',
+    '3 carreau',
+    'valet treffle',
+    '9 cœur',
+    'roi cœur',
+];
+
+dump($cards);
+
+$number = random_int(1, 5);
+
+$removedCards = array_splice($cards, $number - 1, 1);
+$removedCard = $removedCards[0];
+
+dump($number);
+
+echo "La carte n°{$number} retirée est : {$removedCard}";
