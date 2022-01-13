7aa2a972c4080946765e634cb5a92d937bf09d89
jibundeyare <contact@jibundeyare.com>
2022-01-13 11:22:49 +0100

    Amélioration

--- arrays.php
+++ arrays.php
@@ -62,11 +62,21 @@ $fruitsRemoved = array_splice($fruits, 1, 1);
 dump($fruitsRemoved);
 dump($fruits);
 
+// cette syntaxe permet de supprimer un élément à partir de l'index 1
+// mais sans le conserver dans une autre variable
+array_splice($fruits, 1, 1);
+
 // enlever un élément
 // @warning la fonction unset() ne réindexe pas le tableau
 // donc à ne surtout pas utiliser avec une boucle for et un compteur
 // mais elle est plus simple et rapide à exécuter que array_splice()
 unset($fruits[0]);
+
+dump($fruits);
+
+// insérer ou un plusieurs éléments au milieu du tableau
+// insérer à partir de l'index 1, en ne supprimant aucun élément
+array_splice($fruits, 1, 0, ['foo', 'bar', 'baz']);
 
 dump($fruits);
 
@@ -86,6 +96,8 @@ if (in_array('carotte', $bigList)) {
     echo "Il y a des carottes dans la liste<br>\n";
 }
 
+// tableau à clé ou index alphanumérique
+// aussi appelé dictionnaire ou hash map
 $data = [
     'nom' => 'Lorem',
     'prenom' => 'Toto',
@@ -106,4 +118,5 @@ $data['age'] = 12;
 $data['spammer'] = true;
 
 // suppression d'un couple clé valeur
+// @warning on ne peut pas utiliser array_splice() sur un tableau à clé alphanumérique
 unset($data['spammer']);
