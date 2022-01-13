92f80ed2a4367df471c28d541b45bdf52da90c14
jibundeyare <contact@jibundeyare.com>
2022-01-13 12:33:40 +0100

    Amélioration

--- loops.php
+++ loops.php
@@ -1,31 +1,36 @@
 <?php
 
+// cette boucle for affiche les nombres de 0 à 9
 for ($i = 0; $i < 10; $i++) {
     echo "$i<br>\n";
 }
 
 $fruits = ['ananas', 'banane', 'cerise'];
 
+// cette boucle for affiche les tous les éléments d'un tableau à index numérique
 for ($i = 0; $i < count($fruits); $i++) {
     echo "{$i} : {$fruits[$i]}<br>\n";
 }
 
 echo "<br>\n";
 
+// cette boucle for affiche les tous les éléments d'un tableau à index numérique
+// qui ont un numéro d'index pair
 for ($i = 0; $i < count($fruits); $i += 2) {
-    echo "{$fruits[$i]}<br>\n";
+    echo "{$i} : {$fruits[$i]}<br>\n";
 }
 
 echo "<br>\n";
 
-// cette boucle foreach permet d'afficher toutes les valeurs du tableau
+// cette boucle foreach affiche toutes les valeurs d'un tableau
+// @warning cette boucle ne permet pas d'accéder à l'index
 foreach ($fruits as $fruit) {
     echo "$fruit<br>\n";
 }
 
 echo "<br>\n";
 
-// cette boucle foreach permet d'afficher toutes les clés et valeurs du tableau
+// cette boucle foreach affiche toutes les clés et les valeurs d'un tableau
 foreach ($fruits as $key => $value) {
     echo "$key : $value<br>\n";
 }
@@ -38,12 +43,15 @@ $data = [
     'email' => 'toto.titi@example.com',
 ];
 
+// cette boucle foreach affiche toutes les valeurs d'un tableau
+// @warning cette boucle ne permet pas d'accéder à l'index
 foreach ($data as $value) {
     echo "$value<br>\n";
 }
 
 echo "<br>\n";
 
+// cette boucle foreach affiche toutes les clés et les valeurs d'un tableau
 foreach ($data as $key => $value) {
     echo "$key : $value<br>\n";
 }
