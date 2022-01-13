4304ae3ed53c8725597e0cd826b357b8cb1903de
jibundeyare <contact@jibundeyare.com>
2022-01-13 12:27:46 +0100

    Ajout

---
+++ exo-07-loops.php
@@ -0,0 +1,8 @@
+<?php
+
+/*
+Créez toutes les cartes de l'as au roi de cœut en utilisant une boucle for.
+Le résultat doit être un tableau contenant les éléments suivants : ['1 cœur', '2 cœur', '3 cœur', ..., '13 cœur']
+
+Ensuite, utilisez une boucle foreach pour afficher tous les éléments du tableau.
+*/
