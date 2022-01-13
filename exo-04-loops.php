784aba7575b9e8b9103f874a9c8a37319706a379
jibundeyare <contact@jibundeyare.com>
2022-01-13 14:21:49 +0100

    Amélioration

--- exo-04-loops.php
+++ exo-04-loops.php
@@ -1,5 +1,5 @@
 <?php
 
 /*
-Écrivez une boucle for qui compte les nombres de trois en trois, de 0 à 100.
+Écrivez une boucle for qui compte les nombres de trois en trois, en partant de 0 jusqu'à 100.
 */
--- exo-06-loops.php
+++ exo-06-loops.php
@@ -3,6 +3,8 @@
 /*
 Afficher tous les éléments du tableau $fruits avec une boucle foreach.
 Afficher le message "première itération" lors de la première itération et le message "dernière itération" lors de la dernière itération.
+
+Pour déterminer s'il s'agit de la dernière itération, vous devez utiliser la fonction count().
 */
 
 $fruits = ['ananas', 'banane', 'cerise'];
