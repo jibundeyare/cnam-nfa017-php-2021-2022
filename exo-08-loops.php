d88e07baa3189a0c74a0e84315924be62b58c58e
jibundeyare <contact@jibundeyare.com>
2022-01-13 12:32:59 +0100

    Ajout

---
+++ exo-08-loops.php
@@ -0,0 +1,17 @@
+<?php
+
+/*
+Créez toutes les cartes de l'as au roi dans les quatres couleurs en utilisant deux boucles for imbriquées.
+Par convention, nous allons utiliser des nombres pour représenter les couleurs :
+- 1 pour cœur
+- 2 pour carreau
+- 3 pour pique
+- 4 pour treffle
+
+Le résultat doit être un tableau contenant les éléments suivants :
+['1 1', '2 1', '3 1', ..., '13 1', '1 2', '2, 2', '3, 2', ..., '13 2', '1 3', ...]
+  ^                                 ^                                   ^
+  as de cœur                        as de carreau                       as de pique
+
+Ensuite, utilisez deux boucles foreach imbriquées pour afficher tous les éléments du tableau.
+*/
