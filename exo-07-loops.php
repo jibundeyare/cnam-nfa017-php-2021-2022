0e6cbfb2cadf57551b50c0d4b5305dad571e18ce
jibundeyare <contact@jibundeyare.com>
2022-01-14 16:21:23 +0100

    Améliorations

--- exo-07-loops.php
+++ exo-07-loops.php
@@ -1,7 +1,7 @@
 <?php
 
 /*
-Créez toutes les cartes de l'as au roi de cœut en utilisant une boucle for.
+Créez toutes les cartes de l'as jusqu'au roi de cœur en utilisant une boucle for.
 Le résultat doit être un tableau contenant les éléments suivants : ['1 cœur', '2 cœur', ..., '13 cœur']
 
 Ensuite, utilisez une boucle foreach pour afficher tous les éléments du tableau.
--- exo-08-loops.php
+++ exo-08-loops.php
@@ -1,7 +1,7 @@
 <?php
 
 /*
-Créez toutes les cartes de l'as au roi dans les quatre couleurs en utilisant une boucle foreach qui parcours un tableau contenant les quatre couleurs et une boucle for qiu est imbriquée.
+Créez toutes les cartes de l'as jusqu'au roi dans les quatre couleurs, en utilisant une boucle foreach qui parcourt un tableau contenant les quatre couleurs et une boucle for imbriquée.
 
 Une carte est représentée par un tableau à clé alphanumérique, comme ci-dessous avec l'as de cœur :
 [
@@ -9,7 +9,7 @@ Une carte est représentée par un tableau à clé alphanumérique, comme ci-des
     'color: 'cœur',
 ]
 
-Le résultat final doit être un tableau contenant les éléments suivants :
+Le résultat final doit être un tableau comme ci-dessous :
 
 [
     [
@@ -43,7 +43,7 @@ Le résultat final doit être un tableau contenant les éléments suivants :
     ...
 ]
 
-Ensuite, utilisez deux boucles foreach imbriquées pour afficher tous les éléments du tableau.
+Enfin, utilisez une seule boucle foreach pour afficher tous les éléments du tableau.
 */
 
 $colors = ['cœur', 'carreau', 'treffle', 'pique'];
